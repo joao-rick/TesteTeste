af2adfe0a03d7671c3287fac48973640ff86162c
João-rick <almeida.joao@academico.ifrn.edu.br>
2022-10-14 17:37:57 -0300

    Botão

--- aula1/aula1/MainWindow.xaml.cs
+++ aula1/aula1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace aula1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("7w7");
+        }
     }
 }
